0f387a631b5a11705bb2b82b94f429f98666cf9a
zaslse <zaslse@hotmail.com>
2026-07-17 20:35:32 +0000

    Fix Betty space warnings and use hotmail email

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,25 @@
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    unsigned int res = 0;
-    int found_num = 0;
+	int i = 0;
+	int sign = 1;
+	unsigned int res = 0;
+	int found_num = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            res = res * 10 + (s[i] - '0');
-            found_num = 1;
-        }
-        else if (found_num)
-        {
-            break;
-        }
-        i++;
-    }
-    return (res * sign);
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10 + (s[i] - '0');
+			found_num = 1;
+		}
+		else if (found_num)
+		{
+			break;
+		}
+		i++;
+	}
+	return (res * sign);
 }
--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,11 @@
  */
 void print_rev(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (s[len] != '\0')
-        len++;
-    for (len--; len >= 0; len--)
-        _putchar(s[len]);
-    _putchar('\n');
+	while (s[len] != '\0')
+		len++;
+	for (len--; len >= 0; len--)
+		_putchar(s[len]);
+	_putchar('\n');
 }
