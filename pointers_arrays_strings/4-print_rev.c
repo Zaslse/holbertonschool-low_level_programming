16669d503c51acd24f9fe2a90d55dd45e8468405
zaslse <zaslse@student.holberton>
2026-07-17 20:31:49 +0000

    Tasks 0-100: Pointers, arrays and strings

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,10 @@
+#include "main.h"
+void print_rev(char *s)
+{
+    int len = 0;
+    while (s[len] != '\0')
+        len++;
+    for (len--; len >= 0; len--)
+        _putchar(s[len]);
+    _putchar('\n');
+}
